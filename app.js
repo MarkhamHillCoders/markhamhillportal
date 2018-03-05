735a0da6271b94539dc2345753e041f2c31cb410
Daniel <github@profoundideation.com>
2018-03-05 00:52:54 -0500

    update build

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ let path = require('path');
 let favicon = require('serve-favicon');
 let logger = require('morgan');
 let bodyParser = require('body-parser');
-
-
 let profile = require('./routes/profile');
 let app = express();
+
+let mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/markhamhill-portal', { useMongoClient: true, promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('Connection Succesful')).catch((err) => console.error(err));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/profile', profile);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -24,6 +26,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
