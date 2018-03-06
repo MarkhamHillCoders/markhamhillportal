fede815e5489ead7d5e489b8d4fa1b7e031c8fb0
Daniel Lim <hbt@markhamboards.com>
2018-03-06 15:50:52 -0500

    check mess

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Markham Hill Portal'`, async(() => {
+  it(`should have as title 'the Markham Hill Portal'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Markham Hill Portal');
+    expect(app.title).toEqual('the Markham Hill Portal');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the Markham Hill Portal!');
   }));
 });
