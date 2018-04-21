b638aacf7e8ee751d935192b908574bc10712a8a
Daniel <github@profoundideation.com>
2018-04-21 12:33:14 -0400

    edit app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let bodyParser = require('body-parser');
 let profile = require('./routes/profile');
+let directory = require('./routes/directory');
 let app = express();
 
 let mongoose = require('mongoose');
@@ -16,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/profile', profile);
+app.use('/directory', directory);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
