17cd0f5a052a5b8f9fd8d3349c21d5809e7f8073
Daniel <github@profoundideation.com>
2018-04-24 14:31:46 -0400

    update app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import 'rxjs/add/operator/map';
 
+import { Routes, RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
