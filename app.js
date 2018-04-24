5af8af33ae0476288adac97a6ae1bd0b7156e1df
Daniel <github@profoundideation.com>
2018-04-24 15:05:09 -0400

    add express for login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let path = require('path');
 let favicon = require('serve-favicon');
 let logger = require('morgan');
 let bodyParser = require('body-parser');
+let login = require('./routes/login');
 let profile = require('./routes/profile');
 let directory = require('./routes/directory');
 // let livereload = require('express-livereload');
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/profile', profile);
+app.use('/login', login);
 app.use('/directory', directory);
 
 // catch 404 and forward to error handler
