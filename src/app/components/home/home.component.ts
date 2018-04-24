c0ece34dc1b1d82b622c69ef83b8d611680ba49d
Daniel <github@profoundideation.com>
2018-04-24 13:30:56 -0400

    double

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,4 +7,12 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent { }
+
+export class HomeComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
