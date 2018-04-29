bcd68e195c5e0af89f6071ab3e70d9ddd147dc66
Daniel <github@profoundideation.com>
2018-04-29 14:59:26 -0400

    member comp

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-member',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberComponent implements OnInit {
 
-  constructor() { }
+  members: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('/member').subscribe(data => {
+      this.members = data;
+    });
   }
 
 }
