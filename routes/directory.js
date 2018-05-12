8743387378b93b161bdf80221b331639457c51de
Daniel <github@profoundideation.com>
2018-05-12 11:52:12 -0400

    Revert "Kevin"

--- routes/directory.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-/* GET Directory page. */
-router.get('/', function(req, res, next) {
-    res.send('Directory');
-});
-
-module.exports = router;
