e7f14430fd3f9cee40dba85466420bc76ddd3432
Daniel <github@profoundideation.com>
2018-05-12 11:35:15 -0400

    update build

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,31 +8,31 @@ import { Component, OnInit } from '@angular/core';
 
 export class LoginComponent implements OnInit {
 
-  form: FormGroup;
+  // form: FormGroup;
 
-  constructor(private fb: FormBuilder,
-    private authService: AuthService,
-    private router: Router) {
+  // constructor(private fb: FormBuilder,
+  //   private authService: AuthService,
+  //   private router: Router) {
 
-    this.form = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
+  //   this.form = this.fb.group({
+  //     email: ['', Validators.required],
+  //     password: ['', Validators.required]
+  //   });
+  // }
 
-  login() {
-    const val = this.form.value;
+  // login() {
+  //   const val = this.form.value;
 
-    if (val.email && val.password) {
-      this.authService.login(val.email, val.password)
-        .subscribe(
-          () => {
-            console.log("User is logged in");
-            this.router.navigateByUrl('/');
-          }
-        );
-    }
-  }
+  //   if (val.email && val.password) {
+  //     this.authService.login(val.email, val.password)
+  //       .subscribe(
+  //         () => {
+  //           console.log("User is logged in");
+  //           this.router.navigateByUrl('/');
+  //         }
+  //       );
+  //   }
+  // }
 
   ngOnInit() { }
 
