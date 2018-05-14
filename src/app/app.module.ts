d6f1ce14a0d5f97e3a9d07c26968ef007e33c229
Daniel <github@profoundideation.com>
2018-05-14 13:33:17 -0400

    up to date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,6 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
-
 const routes: Routes = [
   { path: 'members', component: UsersComponent },
   { path: 'dashboard', component: DashboardComponent }
